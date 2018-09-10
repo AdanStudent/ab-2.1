2ecf6120f90808bea78f69d258d89f718022c831
adan.benitez <adan.benitez@loop.colum.edu>
2018-09-10 11:38:45 -0500

    Drawing a Rectangle

---
+++ ab-2.1/DrawRectangle.js
@@ -0,0 +1,13 @@
+﻿function main() {
+    var canvas = document.getElementById('example')
+
+    if (!canvas) {
+        console.log('Failed to retrieve the <canvas> element')
+        return
+    }
+
+    var ctx = canvas.getContext('2d')
+
+    ctx.fillStyle = 'rgba(155, 0, 250, 1.0)'
+    ctx.fillRect(120, 10, 150, 150)
+}
